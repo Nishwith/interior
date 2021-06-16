9ddd10c9d2f335ac8bc0e9b578c58d50efcff463
Nishwith <78553323+Nishwith@users.noreply.github.com>
2021-06-16 20:04:54 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById('toggle');
-const sidebar = document.getElementById('sidebar');
-
-document.onclick = function (e) {
-    if(e.target.id !== 'sidebar' && e.target.id !== 'toggle'){
-     sidebar.remove('active');
-    }
-}
